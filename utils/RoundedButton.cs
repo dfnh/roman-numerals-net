f1a2f39c9f98119e4cd0e04b0a20b8922a88c34f
df <defonnh@gmail.com>
2023-02-19 16:05:01 +0300

    rounded button control

---
+++ utils/RoundedButton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace roman_numerals_net.utils
+{
+    public class RoundedButton : Button
+    {
+        [DllImport("gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        static extern IntPtr CreateRoundedButton(int lr, int tr, int rr, int br, int we, int he);
+
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        static extern bool DeleteObject(IntPtr hObject);
+
+        //private int _we = 16;
+        //private int _he = 16;
+        private int _borderRadius = 16;
+
+        //public int WidthEllipse { get => _we; set => _we = value; }
+        //public int HeightEllipse { get => _he; set => _he = value; }
+
+        [Category("Rounded button")]
+        public int BorderRadius { get => _borderRadius; set => _borderRadius = value; }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            IntPtr ptr = CreateRoundedButton(0, 0, Width, Height, BorderRadius, BorderRadius); // WidthEllipse, HeightEllipse
+            Region = Region.FromHrgn(ptr);
+            DeleteObject(ptr);
+        }
+    }
+}
